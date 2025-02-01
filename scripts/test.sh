56b08cea4a2152090484c4a91c46b1e5fa45d10c
anthonyb8 <anthonybaxter819@gmail.com>
2025-01-31 21:07:00 -0500

    Updated bumpversion action.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 
-VENV="venv"
+python() {
+	# Ensure we are in the project root
+	cd "$(dirname "$0")/.." || exit
 
-if [ -f "$VENV/bin/activate" ]; then
-	echo "Virtual environment found. Activating..."
-	source "$VENV/bin/activate"
-else
-	echo "Error: Virtual environment not found at $VENV/bin/activate"
-	exit 1
-fi
+	VENV="venv"
 
-python -m unittest discover
+	# Check if the virtual environment exists
+	if [ ! -d "$VENV" ]; then
+		echo "Error: Virtual environment not found at $VENV"
+		exit 1
+	fi
+
+	# Run Python tests inside the virtual environment directly
+	if cd python; then
+		"$PWD/../venv/bin/python" -m unittest discover
+	fi
+
+}
+
+rust() {
+	if cd rust; then
+		cargo test -- --nocapture
+	fi
+}
+
+options() {
+	echo "Which tests would you like to run?"
+	echo "1 - rust"
+	echo "2 - python"
+
+}
+
+# Main
+while true; do
+	options
+	read -r option
+
+	case $option in
+	1)
+		rust
+		break
+		;;
+	2)
+		python
+		break
+		;;
+
+	*) echo "Please choose a different one." ;;
+	esac
+done
