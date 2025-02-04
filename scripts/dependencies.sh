c26648391f9eaaf696bda451e5378640af07ad2f
anthonyb8 <anthonybaxter819@gmail.com>
2025-02-04 12:02:43 -0500

    UPD: Update mbinary to v1.0.21.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -23,4 +23,7 @@ DEPENDENCIES=$(echo "$DEPENDENCIES" | sed '$ s/,$//')
 # Cleanup temporary file
 rm "$TMPFILE"
 
+# Update requirements.txt
+pip freeze | grep -vE '^(build|twine|bump2version|wheel|setuptools|ibapi[ @])' >python/requirements.txt
+
 echo "Updated pyproject.toml with new dependencies."
